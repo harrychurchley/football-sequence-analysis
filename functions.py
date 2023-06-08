6a0c9e551c156a7ce9c1f4ba9d76449edef12002
harrychurchley <harry_churchley@live.co.uk>
2023-06-08 23:29:34 +0700

    plotting functions added

--- functions.py
+++ functions.py
@@ -47,7 +47,7 @@ def remove_events(df, events):
         df = df[df['type'] != event]
     return df
 
-def map_positions(df)
+def map_positions(df):
     # map positions to broader positions
     position_mapping = {
         'Right Back': 'Defender',
@@ -77,3 +77,69 @@ def map_positions(df)
     }
     df['mapped_position'] = df['position'].map(position_mapping)
     return df
+
+def sort_events(df):
+    df=df.sort_values(by=['match_id', 'index']).reset_index(drop=True)
+    return df
+
+def get_leading_events(df, n_events):
+    shot_events = []
+    event_indexes = []
+    play_outcome = []
+    play_xG = []
+    for i, row in events.iterrows():
+        if row['type'] == 'Shot':
+            outcome = row['shot_outcome']
+            xG = row['shot_statsbomb_xg']
+            start_index = max(0, i - n_events)
+            indexes = list(range(start_index, i))
+            event_indexes.extend(indexes)
+            single_shot_events=[]
+            for j in indexes:
+                event=df['type_player'][j]
+                single_shot_events.append(event)
+                play_outcome.append(outcome)
+                play_xG.append(xG)
+            shot_events.append(single_shot_events)
+    leading_events=df.loc[event_indexes]
+    leading_events['play_outcome'] = play_outcome 
+    leading_events['play_xG'] = play_xG
+    leading_events['play_xG_ranked'] = rankdata(play_xG) / (len(play_xG) + 1)
+    return leading_events
+
+def plot_shot_outcomes(df):
+    # box plot - xG by shot outcome 
+    all_shots=df[df['type']=='Shot']
+    shot_outcome = all_shots['shot_outcome']
+    shot_statsbomb_xg = all_shots['shot_statsbomb_xg']
+
+    # Group the shot_statsbomb_xg data based on shot_outcome
+    data = [shot_statsbomb_xg[shot_outcome == outcome] for outcome in shot_outcome.unique()]
+
+    # Create a horizontal box plot with a larger figure size
+    plt.figure(figsize=(10, 8))
+    box_plot = plt.boxplot(data, labels=shot_outcome.unique(), vert=False)
+
+    # Set labels and title
+    plt.xlabel('Expected Goals (xG)')
+    plt.ylabel('Shot Outcome')
+    plt.title('Box Plot of Shot Outcome vs. Expected Goals')
+
+    # Add counts and mean xG to the plot
+    for i, outcome in enumerate(shot_outcome.unique()):
+        count = len(data[i])
+        mean_xg = np.mean(data[i])
+        plt.text(1.01, i + 1, f'n={count}, mean={mean_xg:.2f}', va='center', ha='left')
+    # Display the plot
+    plt.show()
+
+def plot_shot_envolvements(df):
+    involvements=df['player'].value_counts()
+    plt.figure(figsize=(12, 6))
+    involvements.plot(kind='bar')
+    plt.xlabel('Player')
+    plt.ylabel('Count')
+    plt.title('Involvements in events leading to shots')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
