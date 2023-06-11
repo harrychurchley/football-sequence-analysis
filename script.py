4f314ce97728a9f1b1ecc016b39918c8e47c60f3
harrychurchley <harry_churchley@live.co.uk>
2023-06-11 23:30:31 +0700

    script started

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
-#from statsbombpy import sb
 import pandas as pd
+from statsbombpy import sb
+from functions import select_req_data, clean_player_id, split_location_coords, encode_columns, add_type_player, remove_events, map_positions, sort_events, get_leading_events, plot_shot_outcomes, get_player_involvements, plot_shot_envolvements, get_mode_positions, unpack_positions, get_minutes_played, plot_mins_inv_pos, get_shot_events_list, id_common_event, plot_common_event
 
-#sb.competitions()
+all_events = pd.read_csv("data/2_44_1_all_data.csv")
 
-match = pd.read_csv('data/match1.csv')
+print(all_events.location[129396]) # need to convert str to list. 
