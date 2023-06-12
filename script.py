9886fb01aa11490170e3cafb59675bc8d2fa650b
harrychurchley <harry_churchley@live.co.uk>
2023-06-12 13:51:47 +0700

    clean and prep pipeline implemented

--- script.py
+++ script.py
@@ -1,14 +1,22 @@
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
 from functions import select_req_data, clean_player_id, split_location_coords, encode_columns, add_type_player, remove_events, map_positions, sort_events, get_leading_events, plot_shot_outcomes, plot_shot_envolvements, get_minutes_played, plot_mins_inv_pos, get_shot_events_list, id_common_event, plot_common_event
 
 all_events = pd.read_csv("data/2_44_1_all_data.csv", dtype={'location': object})
-events=select_req_data(all_events, "Arsenal")
-events=clean_player_id(events)
-events=split_location_coords(events, drop_originals=True)
-events=encode_columns(events, columns=['type'], zero_padding=True)
-events=add_type_player(events)
-events=map_positions(events)
-events=sort_events(events)
+
+# clean and prep pipeline
+pipeline = Pipeline([
+    ('select', FunctionTransformer(select_req_data, kw_args={'team': 'Arsenal'})),
+    ('clean', FunctionTransformer(clean_player_id)),
+    ('split', FunctionTransformer(split_location_coords, kw_args={'drop_originals': True})),
+    ('encode', FunctionTransformer(encode_columns, kw_args={'columns': ['type'], 'zero_padding': True})),
+    ('add_type_player', FunctionTransformer(add_type_player)),
+    ('map_positions', FunctionTransformer(map_positions)),
+    ('sort', FunctionTransformer(sort_events))
+])
+
+events = pipeline.transform(all_events)
 leading_events=get_leading_events(events, 10)
 #plot_shot_outcomes(events)
 #plot_shot_envolvements(leading_events)
